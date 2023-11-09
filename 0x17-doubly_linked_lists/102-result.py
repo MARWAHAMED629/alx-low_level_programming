d78d6aef759641a0724a1647e76a0af96a0e35e2
MARWAHAMED629 <mmero385@gmail.com>
2023-11-09 13:49:19 -0800

    update result file

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -2,11 +2,12 @@
 
 largest_palindrome = 0
 
-for x in range(100, 1000):
-    for z in range(100, 1000):
-        product = x * z
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
         if str(product) == str(product)[::-1] and product > largest_palindrome:
             largest_palindrome = product
-# Save  result in  file
+
+# Save the result in a file
 with open("102-result", "w") as file:
     file.write(str(largest_palindrome))
