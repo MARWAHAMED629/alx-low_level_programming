50e3fce629b3da4aca528eeb25c4d5a83d6ca4d0
MARWAHAMED629 <mmero385@gmail.com>
2023-11-17 05:24:57 -0800

    updates operations file

--- 0x18-dynamic_libraries/multi.c
+++ 0x18-dynamic_libraries/multi.c
@@ -1,13 +1,13 @@
 #include "stdlib.h"
 
 /**
- * mul - it is multiply two integer.
+ * multi - it is multiply two integer.
  * @x:iirst int .
  * @i: second int.
  * Return - result multiply
  */
 
-int add(int x, int i)
+int multi(int x, int i)
 {
 return (x * i);
 }
